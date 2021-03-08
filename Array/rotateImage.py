74175f162ef16271922cb348abf9e10b02495a42
Kshitij Bhantagar <32411969+BhatnagarKshitij@users.noreply.github.com>
2021-03-08 23:02:19 +0530

    updated description added reference links

--- Array/rotateImage.py
+++ Array/rotateImage.py
@@ -1,8 +1,10 @@
 '''
-QUESTOIN LINK: https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/770/
+Problem Statement: 
 You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
-
 You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+
+Question Link: https://leetcode.com/problems/rotate-image/
+
 '''
 
 class Solution:
@@ -10,7 +12,7 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        #METHOD-1 #LESS OPTIMIZED
+#         ## METHOD-1 #LESS OPTIMIZED ##
         
 #         lengthOfMatrix=len(matrix)
 #         if lengthOfMatrix <=1:
@@ -34,7 +36,7 @@ class Solution:
 #                 columnRunner=column-1
 #         print(matrix)
 
-        #METHOD -2 OPTIMIZED
+#       ##  METHOD-2 OPTIMIZED ##
         
         lengthOfMatrix=len(matrix)
         common=0
