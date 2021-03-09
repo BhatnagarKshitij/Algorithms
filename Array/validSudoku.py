6fe27142efc2838ea003b701cc76b7e51565ff52
Kshitij Bhantagar <32411969+BhatnagarKshitij@users.noreply.github.com>
2021-03-09 23:47:42 +0530

    LeetCode- Easy Collection -Array

--- Array/validSudoku.py
+++ Array/validSudoku.py
@@ -1,5 +1,5 @@
 '''
-QUESTOIN LINK: https://leetcode.com/problems/valid-sudoku/
+Question link: https://leetcode.com/problems/valid-sudoku/
 
 Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
 
