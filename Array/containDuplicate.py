b3619abe9f6bd9f45da7b76830459ede2ba8119c
Kshitij Bhantagar <32411969+BhatnagarKshitij@users.noreply.github.com>
2021-03-15 22:50:14 +0530

    Updated Contains Duplicate

--- Array/containDuplicate.py
+++ Array/containDuplicate.py
@@ -1,11 +1,14 @@
+'''
+Question link: https://leetcode.com/problems/contains-duplicate/
+
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+'''
+
+
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         oldLength=len(nums)
         newLength=len(set(nums))
-        if oldLength==newLength:
-            return False
-        else:
-            return True
-
+        return False if oldLength == newLength else True
 solution=Solution()
 print(solution.containsDuplicate([]))
