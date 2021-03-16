0c1fb31a3fedc874697b77cd539a8514c86bf139
Kshitij Bhantagar <32411969+BhatnagarKshitij@users.noreply.github.com>
2021-03-16 23:21:01 +0530

    Update rotateArray.py

--- Array/rotateArray.py
+++ Array/rotateArray.py
@@ -1,3 +1,9 @@
+'''
+Question link: https://leetcode.com/problems/rotate-array/
+
+Given an array, rotate the array to the right by k steps, where k is non-negative.
+'''
+
 class Solution:
     def rotate(self, nums, k: int) -> None:
         """
