fd270aa5c1bb8864e1b83175da92b9c4e0c1d9b8
Kshitij Bhantagar <32411969+BhatnagarKshitij@users.noreply.github.com>
2021-03-17 23:24:57 +0530

    Update plusOne.py

--- Array/plusOne.py
+++ Array/plusOne.py
@@ -1,3 +1,12 @@
+'''
+Question link: https://leetcode.com/problems/plus-one/
+
+Given a non-empty array of decimal digits representing a non-negative integer, increment one to the integer.
+
+The digits are stored such that the most significant digit is at the head of the list, and each element in the array contains a single digit.
+
+You may assume the integer does not contain any leading zero, except the number 0 itself.
+'''
 class Solution:
     def plusOne(self, digits):
         
