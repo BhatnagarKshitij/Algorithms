2014cc226c4bf8c5d0e9631904ebfd9bb815247a
Kshitij Bhantagar <32411969+BhatnagarKshitij@users.noreply.github.com>
2021-03-19 23:18:03 +0530

    Update IntegerReverse.py

--- Leetcode/IntegerReverse.py
+++ Leetcode/IntegerReverse.py
@@ -1,3 +1,11 @@
+'''
+Question link: https://leetcode.com/problems/reverse-integer/
+
+Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+
+Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+'''
+
 # def ina(x):
 #     if x<2**31-1 and x>-2**31:
 #         string=str(x)
