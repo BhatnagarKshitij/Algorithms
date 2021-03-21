afdf6a13b606b3db826628c054d4caaf0645520d
Kshitij Bhantagar <32411969+BhatnagarKshitij@users.noreply.github.com>
2021-03-21 23:14:37 +0530

    Updated Unique Characters

--- String/firstUniqueCharacter.py
+++ String/firstUniqueCharacter.py
@@ -1,3 +1,9 @@
+'''
+Question link: https://leetcode.com/problems/first-unique-character-in-a-string/
+
+Given a string, find the first non-repeating character in it and return its index. If it doesn't exist, return -1.
+'''
+
 from collections import Counter
 class Solution:
     def firstUniqChar(self, s: str) -> int:
