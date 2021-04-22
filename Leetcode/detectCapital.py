443f792b3f539a50e8a8c7ae6047df5439fec8a9
Kshitij Bhantagar <32411969+BhatnagarKshitij@users.noreply.github.com>
2021-04-22 22:30:06 +0530

    Update detectCapital.py

--- Leetcode/detectCapital.py
+++ Leetcode/detectCapital.py
@@ -1,3 +1,19 @@
+'''
+Question link: https://leetcode.com/problems/detect-capital/
+
+
+Given a word, you need to judge whether the usage of capitals in it is right or not.
+
+We define the usage of capitals in a word to be right when one of the following cases holds:
+
+All letters in this word are capitals, like "USA".
+All letters in this word are not capitals, like "leetcode".
+Only the first letter in this word is capital, like "Google".
+Otherwise, we define that this word doesn't use capitals in a right way.
+ 
+
+'''
+
 class Solution:
     def detectCapitalUse(self, word: str) -> bool:
         lower=True
