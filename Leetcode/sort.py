b59d7caff771ff4c4b8726b65b74c93a2d3f43e0
Kshitij Bhantagar <32411969+BhatnagarKshitij@users.noreply.github.com>
2021-06-29 23:10:31 +0530

    Create sort.py

---
+++ Leetcode/sort.py
@@ -0,0 +1,14 @@
+'''
+Question link: https://leetcode.com/problems/sort-an-array/
+
+Given an array of integers nums, sort the array in ascending order.
+
+
+
+'''
+
+
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        return sorted(nums)
+        
